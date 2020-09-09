f3caab03a349a1bd926af620f20dabf2dd96f3d2
AAYUSH-GARG-234 <52543500+AAYUSH-GARG-234@users.noreply.github.com>
2020-09-09 18:30:14 +0530

    Update question1.java

--- Assignment_1/question1.java
+++ Assignment_1/question1.java
@@ -1,1 +1,48 @@
+import java.util.Scanner;
+public class question1{
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the textString in lower case : ");
+		String textString=input.next();
+		System.out.println("Enter the substring in lowercase : ");
+		String subString=input.next();
+		int subLength=subString.length();
+		int textStringLenght =textString.length();
+		input.close();
+		System.out.print("Answer is : ");
 
+		if(textStringLenght <subLength)
+		{
+			System.out.println(0);
+			return;
+		}
+		int[] arrayA=new int[26];
+		int ans=0;
+		for(int i=0;i<subLength;i++)
+		{
+			arrayA[textString.charAt(i)-'a']++;
+			arrayA[subString.charAt(i)-'a']--;
+		}
+		if(areAllZeroes(arrayA))
+		{
+			ans++;
+		}
+		for(int i=subLength;i<textStringLenght ;i++)
+		{
+			arrayA[textString.charAt(i-subLength)-'a']--;
+			arrayA[textString.charAt(i)-'a']++;
+			if(areAllZeroes(arrayA))
+				ans++;
+		}
+		System.out.println(ans);
+	}
+	public static boolean areAllZeroes(int[] arrayA)
+	{
+		for(int i=0;i<26;i++)
+		{
+			if(arrayA[i]!=0)
+				return false;
+		}
+		return true;
+	}
+}
